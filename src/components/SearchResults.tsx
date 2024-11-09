25d9c42e9872796e6fc06ac1515d31835f707b56
Andras Nemeseri <andras@nemeseri.comm>
2024-11-08 19:09:00 -0800

    Added cachetime

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -13,6 +13,7 @@ export default function SearchResults({ query }: SearchResultsProps) {
     queryKey: ['search', query],
     queryFn: () => filterCharacter(query),
     staleTime: 1000*60*60*24,
+    gcTime: 1000*60*60*24,
   });
 
   return (<div className='search-results'>
